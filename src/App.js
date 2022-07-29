16ebc7939c68b78dbb40a0dce7d99f20859c727e
m <mikeallison79@gmail.com>
2022-07-28 22:11:18 -0400

    Fixed some testing failues

--- src/App.js
+++ src/App.js
@@ -1,58 +1,64 @@
-import { useEffect, useState } from "react";
-import "./styles.css";
+import { useEffect, useState } from 'react';
+import './styles.css';
 
 export default function App() {
-  const [currentVal, setCurrentVal] = useState("");
+  const [currentVal, setCurrentVal] = useState('');
   const [expression, setExpression] = useState([]);
   const [runningTotal, setRunningTotal] = useState(0);
   const [showRunningTotal, setShowRunningTotal] = useState(false);
 
   const clearClickHandler = () => {
-    setCurrentVal("");
+    setCurrentVal('');
     setExpression([]);
     setRunningTotal(0);
   };
 
-  const numClickHandler = (val) => {
-    setCurrentVal((prevCurrentVal) => {
+  const numClickHandler = val => {
+    if (val === '0' && currentVal.at(-1) === '0') {
+      return;
+    }
+
+    setShowRunningTotal(false);
+
+    setCurrentVal(prevCurrentVal => {
       return prevCurrentVal + val;
     });
   };
 
-  const operClickHandler = (operator) => {
+  const operClickHandler = operator => {
     if (expression.length === 0 && currentVal.length === 0) {
       return;
     }
 
     if (
       currentVal.length === 0 &&
-      ["+", "-", "*", "/"].includes(expression.at(-1))
+      ['+', '-', '*', '/'].includes(expression.at(-1))
     ) {
-      setExpression((prevExpression) => {
+      setExpression(prevExpression => {
         return prevExpression.slice(0, -1) + operator;
       });
       return;
     }
 
-    setExpression((prevExpression) => {
+    setExpression(prevExpression => {
       return prevExpression.concat(currentVal);
     });
-    setExpression((prevExpression) => {
+    setExpression(prevExpression => {
       return prevExpression.concat(operator);
     });
-    setCurrentVal("");
+    setCurrentVal('');
   };
 
   const decimalClickHandler = () => {
-    if (currentVal.toString().includes(".")) {
+    if (currentVal.toString().includes('.')) {
       return;
     }
 
     if (currentVal.length === 0) {
-      setCurrentVal("0.");
+      setCurrentVal('0.');
     } else {
-      setCurrentVal((prevCurrentVal) => {
-        return prevCurrentVal + ".";
+      setCurrentVal(prevCurrentVal => {
+        return prevCurrentVal + '.';
       });
     }
   };
@@ -63,16 +69,16 @@ export default function App() {
     for (let i = 1; i < expression.length; i++) {
       if (expression[i + 1]) {
         switch (expression[i]) {
-          case "+":
+          case '+':
             total += Number(expression[i + 1]);
             break;
-          case "-":
+          case '-':
             total -= Number(expression[i + 1]);
             break;
-          case "*":
+          case '*':
             total *= Number(expression[i + 1]);
             break;
-          case "/":
+          case '/':
             total /= Number(expression[i + 1]);
             break;
           default:
@@ -82,70 +88,76 @@ export default function App() {
     }
 
     setRunningTotal(total);
+
+    console.log('running total', runningTotal);
+    console.log('current val', currentVal);
+    console.log('showRunningTotal', showRunningTotal);
   });
 
   const calcClickHandler = () => {
-    setExpression((prevExpression) => {
+    setExpression(prevExpression => {
       return prevExpression.concat(currentVal);
     });
     setShowRunningTotal(true);
+    setCurrentVal('');
+    // setExpression([]);
   };
 
   return (
     <div className="App">
       <div id="container">
         <div>Running Total: {runningTotal}</div>
-        <div id="expression">Expression: {expression}</div>
-        <div id="display">
-          Result: {showRunningTotal ? runningTotal : currentVal}
-        </div>
+        Expression:
+        <div id="expression">{expression}</div>
+        Current Val:
+        <div id="display">{showRunningTotal ? runningTotal : currentVal}</div>
         <div id="buttons">
           <button id="clear" onClick={clearClickHandler}>
             AC
           </button>
-          <button id="divide" onClick={operClickHandler.bind(this, "/")}>
+          <button id="divide" onClick={operClickHandler.bind(this, '/')}>
             /
           </button>
-          <button id="multiply" onClick={operClickHandler.bind(this, "*")}>
+          <button id="multiply" onClick={operClickHandler.bind(this, '*')}>
             X
           </button>
-          <button id="seven" onClick={numClickHandler.bind(this, 7)}>
+          <button id="seven" onClick={numClickHandler.bind(this, '7')}>
             7
           </button>
-          <button id="eight" onClick={numClickHandler.bind(this, 8)}>
+          <button id="eight" onClick={numClickHandler.bind(this, '8')}>
             8
           </button>
-          <button id="nine" onClick={numClickHandler.bind(this, 9)}>
+          <button id="nine" onClick={numClickHandler.bind(this, '9')}>
             9
           </button>
-          <button id="subtract" onClick={operClickHandler.bind(this, "-")}>
+          <button id="subtract" onClick={operClickHandler.bind(this, '-')}>
             -
           </button>
-          <button id="four" onClick={numClickHandler.bind(this, 4)}>
+          <button id="four" onClick={numClickHandler.bind(this, '4')}>
             4
           </button>
-          <button id="five" onClick={numClickHandler.bind(this, 5)}>
+          <button id="five" onClick={numClickHandler.bind(this, '5')}>
             5
           </button>
-          <button id="six" onClick={numClickHandler.bind(this, 6)}>
+          <button id="six" onClick={numClickHandler.bind(this, '6')}>
             6
           </button>
-          <button id="add" onClick={operClickHandler.bind(this, "+")}>
+          <button id="add" onClick={operClickHandler.bind(this, '+')}>
             +
           </button>
-          <button id="one" onClick={numClickHandler.bind(this, 1)}>
+          <button id="one" onClick={numClickHandler.bind(this, '1')}>
             1
           </button>
-          <button id="two" onClick={numClickHandler.bind(this, 2)}>
+          <button id="two" onClick={numClickHandler.bind(this, '2')}>
             2
           </button>
-          <button id="three" onClick={numClickHandler.bind(this, 3)}>
+          <button id="three" onClick={numClickHandler.bind(this, '3')}>
             3
           </button>
           <button id="equals" onClick={calcClickHandler}>
             =
           </button>
-          <button id="zero" onClick={numClickHandler.bind(this, 0)}>
+          <button id="zero" onClick={numClickHandler.bind(this, '0')}>
             0
           </button>
           <button id="decimal" onClick={decimalClickHandler}>
